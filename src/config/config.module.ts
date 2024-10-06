cb14bab874fc400488748ebb275f46000a1e1280
Damien Arnaud <damien.arnaudpro@gmail.com>
2024-10-06 19:01:53 +0200

    Database configuration

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,8 @@
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import db_configuration from './db_configuration';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
@@ -8,6 +10,23 @@ import { Module } from '@nestjs/common';
       isGlobal: true,
       load: [db_configuration],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+        dropSchema: true,
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
   ],
 })
 export class AppConfigModule {}
--- src/config/db_configuration.ts
+++ src/config/db_configuration.ts
@@ -5,6 +5,6 @@ export default () => ({
     port: parseInt(process.env.DB_PORT || '3306', 10),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    name: process.env.DB_NAME,
+    database: process.env.DB_NAME,
   },
 });
